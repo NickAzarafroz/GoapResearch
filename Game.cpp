64e71df1e1333e5323d1265df4640f5ed68e8a08
Nicckx <71626472+NickAzarafroz@users.noreply.github.com>
2023-08-25 13:12:10 +0200

    Add project files.

---
+++ Game.cpp
@@ -0,0 +1,91 @@
+#include "pch.h"
+#include "Game.h"
+#include <iostream>
+
+Game::Game( const Window& window ) 
+	:m_Window{ window }
+{
+	Initialize( );
+}
+
+Game::~Game( )
+{
+	Cleanup( );
+}
+
+void Game::Initialize( )
+{
+
+}
+
+void Game::Cleanup( )
+{
+	
+}
+
+void Game::Update(float elapsedSec)
+{
+
+}
+
+void Game::Draw( ) const
+{
+	ClearBackground();
+}
+
+void Game::ProcessKeyDownEvent( const SDL_KeyboardEvent & e )
+{
+	//std::cout << "KEYDOWN event: " << e.keysym.sym << std::endl;
+}
+
+void Game::ProcessKeyUpEvent( const SDL_KeyboardEvent& e )
+{
+	//std::cout << "KEYUP event: " << e.keysym.sym << std::endl;
+}
+
+void Game::ProcessMouseMotionEvent( const SDL_MouseMotionEvent& e )
+{
+	//std::cout << "MOUSEMOTION event: " << e.x << ", " << e.y << std::endl;
+}
+
+void Game::ProcessMouseDownEvent( const SDL_MouseButtonEvent& e )
+{
+	//std::cout << "MOUSEBUTTONDOWN event: ";
+	//switch ( e.button )
+	//{
+	//case SDL_BUTTON_LEFT:
+	//	std::cout << " left button " << std::endl;
+	//	break;
+	//case SDL_BUTTON_RIGHT:
+	//	std::cout << " right button " << std::endl;
+	//	break;
+	//case SDL_BUTTON_MIDDLE:
+	//	std::cout << " middle button " << std::endl;
+	//	break;
+	//}
+}
+
+void Game::ProcessMouseUpEvent( const SDL_MouseButtonEvent& e )
+{
+	//std::cout << "MOUSEBUTTONUP event: ";
+	//switch ( e.button )
+	//{
+	//case SDL_BUTTON_LEFT:
+	//	std::cout << " left button " << std::endl;
+	//	break;
+	//case SDL_BUTTON_RIGHT:
+	//	std::cout << " right button " << std::endl;
+	//	break;
+	//case SDL_BUTTON_MIDDLE:
+	//	std::cout << " middle button " << std::endl;
+	//	break;
+	//}
+}
+
+void Game::ClearBackground( ) const
+{
+	glClearColor(0.0f, 1.0f, 1.f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT);
+}
+
+
